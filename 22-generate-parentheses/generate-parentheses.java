211626f51681a458419edd5ed6a338eac60fc3e9
Mr. Uzair <62235689+uzairdev07@users.noreply.github.com>
2024-03-29 07:17:42 +0500

    Time: 1 ms (84.99%) | Memory: 43.5 MB (23.35%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {    
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        generateParenthesis(result, "", 0, 0, n);
+        return result;
+    }
+    
+    private void generateParenthesis(List<String> result, String current, int open, int close, int max) {
+        if (current.length() == max * 2) {
+            result.add(current);
+            return;
+        }
+        
+        if (open < max) {
+            generateParenthesis(result, current + "(", open + 1, close, max);
+        }
+        
+        if (close < open) {
+            generateParenthesis(result, current + ")", open, close + 1, max);
+        }
+    }
+    
+
+}
