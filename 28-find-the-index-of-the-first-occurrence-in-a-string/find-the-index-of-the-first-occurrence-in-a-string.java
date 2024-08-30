e5166e9280249239232133f5130058c2c95a3253
Mr. Uzair <62235689+uzairdev07@users.noreply.github.com>
2024-08-30 16:00:13 +0500

    Time: 0 ms (100.00%) | Memory: 41.7 MB (12.97%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,18 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
+    public int strStr(String a, String b) {
+        // return haystack.indexOf(needle);
+        int l1 = a.length(), l2 = b.length();
+
+        if (l2 == 0)
+            return 0;
+        
+        if (l1 < l2)
+            return -1;
+
+        for (int i = 0; i <= l1 - l2; i++) {
+            if (a.substring(i, i + l2).equals(b))
+                return i;
+        }
+        return -1;
     }
 }
