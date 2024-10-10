9fa58f419aac573583b9e83caa7dd136373b159a
Mr. Uzair <62235689+uzairdev07@users.noreply.github.com>
2024-10-10 14:41:49 +0500

    Time: 0 ms (100.00%) | Memory: 40.2 MB (57.44%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.java
+++ 2507-number-of-common-factors/number-of-common-factors.java
@@ -1,7 +1,8 @@
 class Solution {
     public int commonFactors(int a, int b) {
         int c = 0;
-        for (int i = 1; i <= Math.min(a, b); i++)
+        int min = Math.min(a, b);
+        for (int i = 1; i <= min; i++)
             if ((a % i == 0) && (b % i == 0))
                 c++;
         return c;
