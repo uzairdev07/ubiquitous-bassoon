c18ae18eff5ea134c4f176517c63ab81f1908c60
Mr. Uzair <62235689+uzairdev07@users.noreply.github.com>
2024-10-13 07:20:06 +0500

    Time: 136 ms (75.06%) | Memory: 41.6 MB (50.66%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0 || word == null || word.length() == 0) {
+            return false;
+        }
+        
+        int m = board.length;
+        int n = board[0].length;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, 0, i, j)) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int index, int i, int j) {
+        if (index == word.length()) {
+            return true;
+        }
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) {
+            return false;
+        }
+        
+        char temp = board[i][j];
+        board[i][j] = '#'; // Mark as visited
+        
+        boolean found = dfs(board, word, index + 1, i + 1, j) ||
+                        dfs(board, word, index + 1, i - 1, j) ||
+                        dfs(board, word, index + 1, i, j + 1) ||
+                        dfs(board, word, index + 1, i, j - 1);
+        
+        board[i][j] = temp; // Restore the original value
+        return found;
+    }
+}
