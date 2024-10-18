a01d98739ae27ae505837c5032ae0d1d490ca38b
Mr. Uzair <62235689+uzairdev07@users.noreply.github.com>
2024-10-18 18:05:36 +0500

    Time: 16 ms (45.20%) | Memory: 45.5 MB (65.44%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,30 +1,17 @@
 class Solution {
     public String frequencySort(String s) {
-        Map<Character, Integer> data = new HashMap<>();
-        
-        char[] chars = s.toCharArray();
-        
-        for (char ch : chars)
-            data.put(ch, data.getOrDefault(ch, 0) + 1);
-        
-        data = data.entrySet()
-                  .stream()
-                  .sorted((i1, i2)
-                              -> i2.getValue().compareTo(
-                                  i1.getValue()))
-                  .collect(Collectors.toMap(
-                      Map.Entry::getKey,
-                      Map.Entry::getValue,
-                      (e1, e2) -> e1, LinkedHashMap::new));
-        
-        String result = "";
-        for (Character ch : data.keySet()) {
-            for (int i = 0; i < data.get(ch); i++) {
-                result += ch;
-            }
-        }
-        
-        return result;
-        
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : s.toCharArray())
+            freq.put(c, freq.getOrDefault(c, 0)+1);
+
+        List<Map.Entry<Character, Integer>> sorted = 
+            new ArrayList<>(freq.entrySet());
+
+        Collections.sort(sorted, (o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+
+        String res = "";
+        for (Map.Entry<Character, Integer> each : sorted)
+            res += String.valueOf(each.getKey()).repeat(each.getValue());
+        return res;
     }
 }
