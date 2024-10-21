9dbb5dd2b4418c2b8c5a36cb71994580677886b8
Mr. Uzair <62235689+uzairdev07@users.noreply.github.com>
2024-10-21 07:05:25 +0500

    Time: 1 ms (100.00%) | Memory: 42.4 MB (51.95%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -2,28 +2,29 @@ class Solution {
     public int myAtoi(String s) {
         s = s.trim();
 
-        if (s.isEmpty())
+        if (s.length() == 0)
             return 0;
         
-        int ans = 0, i = 0;
-
         boolean neg = s.charAt(0) == '-';
         boolean pos = s.charAt(0) == '+';
 
-        if (neg || pos)
-            i++;
+        int i = (neg || pos) ? 1 : 0;
+        return solve(s, i, 0, neg);
 
-        while (i < s.length() && Character.isDigit(s.charAt(i))) {
-
-            int digit = s.charAt(i) - '0';
-
-            if (ans > Integer.MAX_VALUE/10 || (ans == Integer.MAX_VALUE/10 && digit > Integer.MAX_VALUE%10)) {
-                return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-            }
-
-            ans = ans * 10 + digit;
-            i++;
-        }
-        return neg ? -ans : ans;
     }
+
+    int solve(String s, int i, int res, boolean neg) {
+        if (i >= s.length() || !Character.isDigit(s.charAt(i)))
+            return neg ? -res : res;
+
+        int digit = s.charAt(i)-'0';
+        
+        if (res > Integer.MAX_VALUE/10 || (res == Integer.MAX_VALUE/10 && digit > Integer.MAX_VALUE%10))
+            return neg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+        res = res * 10 + digit;
+        
+        return solve(s, i+1, res, neg);
+    }
+
 }
